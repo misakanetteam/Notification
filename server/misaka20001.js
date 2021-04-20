74bc60d396ada9d8d25d59d9fb61f16c5f330cbe
misaka19465 <misaka19465@olddoctor.net>
2021-04-20 23:18:52 +0800

    debug update

--- server/misaka20001.js
+++ server/misaka20001.js
@@ -40,7 +40,7 @@ logger.info("got misakaKeys");
 logger.info("start server");
 require("http").createServer(function(req, res) {
     logger.info("new client");
-    let misakaKey = req.headers["misakaKey"];
+    let misakaKey = req.headers["misaka-key"];
     logger.debug(req.headers);
     let position;
     if ((position = misakaKeys[misakaKey]) == undefined) {
